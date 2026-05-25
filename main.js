d82738e88ed8ac6ed568dba03250bc06650eb099
nath0402 <lino.nathalia@escola.pr.gov.br>
2026-05-25 08:11:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
+for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
@@ -15,9 +15,20 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-12-18T00:00:00");
+const tempoObjetivo2 = new Date("2026-05-31T00:00:00");
+const tempoObjetivo3 = new Date("2026-08-17T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-28T00:00:00");
 
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
 
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
